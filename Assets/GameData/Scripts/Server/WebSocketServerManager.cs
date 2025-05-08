d51eae8a67b5938a8f227b14060f947d7ad19d7d
imnixan <vcmvenom@gmail.com>
2025-05-09 00:57:10 +0700

    websockets work on deploy

--- Assets/GameData/Scripts/Server/WebSocketServerManager.cs
+++ Assets/GameData/Scripts/Server/WebSocketServerManager.cs
@@ -17,7 +17,19 @@ namespace PJTC.Server
         void OnEnable()
         {
             wss = new WebSocketServer(System.Net.IPAddress.Any, 8080, true);
-            wss.SslConfiguration.ServerCertificate = new X509Certificate2();
+            try
+            {
+                wss.SslConfiguration.ServerCertificate = new X509Certificate2(
+                    "/etc/letsencrypt/live/pctc.wowandy.dev/certificate.pfx",
+                    "1337",
+                    X509KeyStorageFlags.DefaultKeySet
+                );
+                Debug.Log("Certificate loaded successfully");
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError($"Failed to load certificate: {ex.Message}\n{ex.StackTrace}");
+            }
             wss.AddWebSocketService<PlayerListener>("/checkers"); // Подключение по wss://pctc.wowandy.dev:8081/ws
             wss.Start();
             Debug.Log($"{wss.Address} is listening on port {wss.Port}");
